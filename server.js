349990a643201d723a852368aa85bb93dd30ba3d
marcos henrique <marcosmarcondes700@gmail.com>
2020-11-27 16:46:08 -0300

    Texto

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
-const port = process.env.port || 8080;
+const port = process.env.port || 8081;
 
 server.use(middlewares);
 server.use(router);
 server.listen(port, ()=>{
-    console.log('JSON SERVER is running in ${port}');   
+    console.log(`JSON SERVER is running in ${port}`);   
 });
