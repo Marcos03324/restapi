edc2aae69e1bf161d97396986d5b949b88161765
marcos henrique <marcosmarcondes700@gmail.com>
2020-11-27 15:51:39 -0300

    Configuração

---
+++ server.js
@@ -0,0 +1,13 @@
+const ksonServer = require('json-server');
+
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+const port = process.env.port || 8080;
+
+server.use(middlewares);
+server.use(router);
+server.listem(port, ()=>{
+    console.log('JSON SERVER is running in ${port}');
+});
